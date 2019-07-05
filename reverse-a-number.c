210714fafec290287d65f943687172eaa262ee64
David Losas <45861490+Electrobite@users.noreply.github.com>
2019-07-05 18:39:49 +0200

    First Commit

--- reverse-a-number.c
+++ reverse-a-number.c
@@ -2,21 +2,18 @@
  
 int main() {
  
-    int numero; // Número introducido por el usuario.
-    int resto;  // Almacena el resto de la división.     
+    int num;
+    int rest;  
  
-    printf("Ingrese el numero que desea invertir: ");             
-    scanf("%i", &numero); // Escanea el número introducido. No olvidarse de "&".
-    printf("\n"); // Salto de línea.
-    printf("Su numero invertido es: ");
+    printf("Set an integer number: ");             
+    scanf("%i", &number); 
+    printf("\n"); 
+    printf("Your reversed number is: ");
  
-    // Mientras que "numero sea mayor que "0" hacer:
-    
-    while(numero > 0) {
- 
-         resto = numero % 10;  // Obtenemos el resto de la división.                
-         numero = numero / 10; // Obtenemos el nuevo número.   
-         printf("%i", resto);  // Imprimimos el resto.
+    while(number > 0) {
+         rest = num % 10;             
+         num = num / 10; 
+         printf("%i", rest);  
     }
  
     return 0;
