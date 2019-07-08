9dedf8b46a25f06f93ef7e4828b754b00000b361
David Losas <45861490+Electrobite@users.noreply.github.com>
2019-07-08 12:09:48 +0200

    First Commit

--- decimal-to-binary-octal.c
+++ decimal-to-binary-octal.c
@@ -14,31 +14,30 @@ int main () {
     printf ("\n");  
  
     while (option < 1 || option > 2) {   
-	   printf ("Set the type of conversion: binary (1), octal (2): ");	
-	   scanf ("%i", &option);
-	   printf ("\n"); 
+        printf ("Set the type of conversion: binary (1), octal (2): ");	
+        scanf ("%i", &option);
+        printf ("\n"); 
     }
  
     if (option == 1) {
-	   printf ("The number %i in decimal is equivalent to binary: ", number);
-       base = 2;
+        printf ("The number %i in decimal is equivalent to binary: ", number);
+        base = 2;
     }
  
     if (option == 2) {
-	   printf ("The number %i in decimal is equivalent to octal: ", number);
-       base = 8;
+        printf ("The number %i in decimal is equivalent to octal: ", number);
+        base = 8;
     }
  
     for ( a = 0 ; a < 50 ; a++ ) { 
-       rest[a] = number % base;                         
-       number = number / base; 
-       if (number == 0) break;    
+        rest[a] = number % base;                         
+        number = number / base; 
+        if (number == 0) break;    
     }
     
     for ( b = a ; b >= 0 ; b-- ) {
-		printf ("%i", rest[b]); 
+        printf ("%i", rest[b]); 
     } 
  
     return 0;
 }
-
